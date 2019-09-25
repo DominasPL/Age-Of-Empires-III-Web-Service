0176220bd5f6c57705a337e7253034a9256f0822
DomiasPL <dominik-stepuch@wp.pl>
2019-09-25 19:24:40 +0200

    Start of the project. Added neccessary dependencies

---
+++ src/main/java/com/github/dominaspl/aoewebservice/AoewebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.dominaspl.aoewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoewebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoewebserviceApplication.class, args);
+    }
+
+}
